7233c91f3174bc3016c31477a74b012b417cdae5
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-07-26 01:35:46 +0530

    Update Q1.cpp

--- Chapter/Ch2/Q1.cpp
+++ Chapter/Ch2/Q1.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    
+    int a , b , c ;
+    cout <<" Enter the marks : "<< endl;
+    cin >> a >> b >> c ;
+    cout << setw(50) << "Maths = "<< a << endl << setw(50) << "Physics = "<< b <<endl<<setw(50) << "Chemistry = "<< c << endl ;
+	return 0;
 
+}
