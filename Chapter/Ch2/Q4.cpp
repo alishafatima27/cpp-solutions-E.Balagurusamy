0c95be7a9a845a4f7c967d6e7176dc5b02c4b3f6
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-07-26 01:37:44 +0530

    Create Q4.cpp

---
+++ Chapter/Ch2/Q4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a , b , c ;
+    float x ; 
+    cout << "Enter value of a , b , c " << endl ; 
+    cin >> a >> b >> c ;
+    x = a/b - c ; 
+    cout << x ; 
+}
