bc0986159cb54d838bd5fb76e6725d1b3d621d32
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-07-27 01:51:52 +0530

    Create Q5.cpp

---
+++ Chapter/Ch3/Q5.cpp
@@ -0,0 +1,8 @@
+/* 
+An election is contested by five candidates. The candidates are numbered 1 to 5 and 
+the voting is done by marking the candidate number on the ballot paper. 
+  
+Write a program to read the ballots and count the votes cast for each candidate using an array variable count. 
+In case, a number read is outside the range 1 to 5, the ballot should be considered as a 'spoil ballot', and 
+the program should also count the number of spoiled ballots. 
+*/ 
