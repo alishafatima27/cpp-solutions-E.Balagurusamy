fbf56a4a8177abad7b277f015798cfbc36e2c7eb
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-07-31 17:31:44 +0530

    Update Q9.cpp

--- Chapter/Ch3/Q9.cpp
+++ Chapter/Ch3/Q9.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate the sum of elements in an array
+int sum_n(int x, int data[])
+{
+    int sum = 0;
+    for (int i = 0; i < x; i++)
+    {
+        sum += data[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+
+    // Step 1: Input the size of the dataset
+    cout << "Enter the size of the dataset: ";
+    cin >> n;
+
+    // Declare arrays to store data and squared data
+    int data[n], sq_data[n];
+
+    // Step 2: Input the data points
+    cout << "Enter " << n << " data points:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data " << i + 1 << ": ";
+        cin >> data[i];
+        sq_data[i] = data[i] * data[i]; // Calculate squared data
+    }
+
+    // Step 3: Calculate the sum of the data points
+    int sum = sum_n(n, data);
+
+    // Step 4: Calculate the mean (expectation) of the numbers
+    float exp = static_cast<float>(sum) / n;
+
+    // Step 5: Display the mean (expectation) of the numbers
+    cout << "Mean/expectation of the numbers: " << exp << endl;
+
+    // Step 6: Calculate the sum of squared data points
+    float exp_sq = sum_n(n, sq_data);
+
+    // Step 7: Calculate the mean (expectation) of squared numbers
+    float exp_sq_mean = exp_sq / n;
+
+    // Step 8: Display the mean (expectation) of squared numbers
+    cout << "Mean/expectation of squared numbers: " << exp_sq_mean << endl;
+
+    // Step 9: Calculate the variance
+    float var = exp_sq_mean - exp * exp;
+
+    // Step 10: Display the variance
+    cout << "Variance: " << var << endl;
+
+    return 0;
+}
 
