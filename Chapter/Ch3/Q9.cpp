59332eaa8e4b20b35c1a0e4e2b8166065f3fa044
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-07-31 23:02:03 +0530

    Update Q9.cpp

--- Chapter/Ch3/Q9.cpp
+++ Chapter/Ch3/Q9.cpp
@@ -1,3 +1,28 @@
+/*     variance = E(x^2) - (E(x))^2
+Approach to Calculate Variance:
+
+Break down the variance formula into two parts:
+
+Calculate the expectation of squared data points, i.e., E(x^2).
+Calculate the summation of data points and find the mean (expectation), i.e., (E(x))^2.
+Take user input for the size of the dataset (n) and the data points.
+
+Create a function called sum_n to calculate the summation of elements in an array. Pass the array data as an argument to this function.
+
+Use the sum_n function twice:
+
+First, to calculate the summation of the data points (E(x)).
+Second, to calculate the summation of the squared data points (E(x^2)).
+After obtaining the summation of data points and squared data points, calculate the mean (expectation) by dividing the summation by the number of data points (n).
+
+Declare a variable called var to store the variance value.
+
+Use the formula var = E(x^2) - (E(x))^2 to calculate the variance.
+
+Display the variance value as the final result.
+/*
+// Note, Here I have used my own personal approach to solve the problem. I have not used the formula given in the book.
+    
 #include <iostream>
 using namespace std;
 
@@ -54,7 +79,7 @@ int main()
     float var = exp_sq_mean - exp * exp;
 
     // Step 10: Display the variance
-    cout << "Variance: " << var << endl;
+    cout << "Variance = " << var << endl;
 
     return 0;
 }
