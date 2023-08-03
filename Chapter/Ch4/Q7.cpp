7d28997c92cb5cfb3c08a4bcc9365797c18792e5
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-03 23:59:46 +0530

    Create Q7.cpp

---
+++ Chapter/Ch4/Q7.cpp
@@ -0,0 +1,4 @@
+/* Write a function power() to raise a number m to power n. The function takes a double value for m and int n,
+   and returns the result correctly. 
+  Use default value of 2 for n to make the function calculate squares when this argument is ommitted.
+  Write a main that gets a values of m and n from the user to test the function.
