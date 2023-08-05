760695a31a322393c62f84cee9cae4956b137df5
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-05 20:07:48 +0530

    Create Q1.cpp

---
+++ Chapter/Ch5/Q1.cpp
@@ -0,0 +1,18 @@
+/* Define a class to represent a bank account. Include the following members:
+
+    Data members
+
+      (a) Name of the depositor
+      (b) Account number
+      (c) Type of account
+      (d) Balance amount in the account
+
+    Member functions
+
+      (a) To assign initial values
+      (b) To deposit an amount
+      (c) To withdraw an amount after checking the balance
+      (d) To display name and balance
+
+  Write a main program to test the program.
+*/ 
