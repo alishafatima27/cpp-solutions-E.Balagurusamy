edf089ea63c7949e75efb21e064940aad346d384
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-08 23:21:33 +0530

    Create Q4.cpp

---
+++ Chapter/Ch5/Q4.cpp
@@ -0,0 +1,13 @@
+/* Modify the class and program of Exercise 5.2 such that the program would be able to add two vectors 
+   and display the resultant vector. (Note that we can pass objects as function arguments.) 
+*/
+
+/*Q2  Write a class to represent a vector (a series of float values). Include member functions to perform the following tasks:
+
+   (a) To create a vector
+   (b) To modify the value of a given element
+   (c) To multiply by a scalar value
+   (d) To display the vector in the form (10, 20, 30, ...)
+
+   Write a program to test your class.
+*/ 
