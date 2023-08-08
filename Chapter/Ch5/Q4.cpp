cd701f3f74f3115570901efbf75f5e9667982f31
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-08 23:58:22 +0530

    Update Q4.cpp

--- Chapter/Ch5/Q4.cpp
+++ Chapter/Ch5/Q4.cpp
@@ -11,15 +11,7 @@
 
    Write a program to test your class.
 */ 
-/* Write a class to represent a vector (a series of float values). Include member functions to perform the following tasks:
 
-   (a) To create a vector
-   (b) To modify the value of a given element
-   (c) To multiply by a scalar value
-   (d) To display the vector in the form (10, 20, 30, ...)
-
-   Write a program to test your class.
-*/ 
 #include <iostream>
 #include <vector>
 using namespace std;
