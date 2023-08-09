4714637fee14cb84f9368746ca39a432c70814c9
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-09 22:54:43 +0530

    Create Q5.cpp

---
+++ Chapter/Ch5/Q5.cpp
@@ -0,0 +1,9 @@
+/*Create two classes DM and DB which store the value of distances. 
+  DM stores distances in meters and centimeters and 
+  DB in feet and inches. 
+  Write a program that can read values for the class objects and add one object of DM with another object of DB.
+
+Use a friend function to carry out the addition operation. 
+The object that stores the results may be a DM object or DB object, depending on the units in which the results are required.
+The display should be in the format of feet and inches or meters and centimeters depending on the object on display.
+*/
