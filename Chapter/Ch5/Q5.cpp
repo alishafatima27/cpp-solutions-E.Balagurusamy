fac75a691bed98d28ff5402d23d94c7d24dbfb30
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-09 22:58:36 +0530

    Update Q5.cpp

--- Chapter/Ch5/Q5.cpp
+++ Chapter/Ch5/Q5.cpp
@@ -7,3 +7,71 @@ Use a friend function to carry out the addition operation.
 The object that stores the results may be a DM object or DB object, depending on the units in which the results are required.
 The display should be in the format of feet and inches or meters and centimeters depending on the object on display.
 */
+
+#include <iostream>
+using namespace std;
+
+class DB;
+
+class DM {
+    int meters;
+    int centimeters;
+
+public:
+    void getDistance() {
+        cout << "Enter distance in meters: ";
+        cin >> meters;
+        cout << "Enter distance in centimeters: ";
+        cin >> centimeters;
+    }
+
+    void display() {
+        cout << "Distance: " << meters << " meters and " << centimeters << " centimeters" << endl;
+    }
+
+    friend DM add(DM, DB);
+};
+
+class DB {
+    int feet;
+    int inches;
+
+public:
+    void getDistance() {
+        cout << "Enter distance in feet: ";
+        cin >> feet;
+        cout << "Enter distance in inches: ";
+        cin >> inches;
+    }
+
+    void display() {
+        cout << "Distance: " << feet << " feet and " << inches << " inches" << endl;
+    }
+
+    friend DM add(DM, DB);
+};
+
+DM add(DM dm, DB db) {
+    DM result;
+    int totalCentimeters = (dm.meters * 100 + dm.centimeters) + ((db.feet * 12 + db.inches) * 2.54);
+    
+    result.meters = totalCentimeters / 100;
+    result.centimeters = totalCentimeters % 100;
+
+    return result;
+}
+
+int main() {
+    DM dmObj;
+    DB dbObj;
+    dmObj.getDistance();
+    dbObj.getDistance();
+
+    DM result = add(dmObj, dbObj);
+
+    cout << "Sum of distances:" << endl;
+    result.display();
+
+    return 0;
+}
+
