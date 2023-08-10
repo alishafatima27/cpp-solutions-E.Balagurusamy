eeca2b6cfc54e8e3d919bab955e65bb2d818ca8d
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-11 01:08:55 +0530

    Update Q6.cpp

--- Chapter/Ch5/Q6.cpp
+++ Chapter/Ch5/Q6.cpp
@@ -2,34 +2,106 @@
      returns new matrix object containing their multiplication result.
 */
 
+/* Apprach for the question : 
+
+   Let's say we have two matrices, A and B:
+| 1  2  3 |      
+| 4  5  6 |
+| 7  8  9 |
+     and 
+| 9  8  7 |
+| 6  5  4 |
+| 3  2  1 |
+We analyse that when we multiply A*B :
+C[i][j] = A[i][0]*B[0][j] + A[i][1]*B[1][j] + A[i][2]*B[2][j]
+
+ So For each element in the resulting matrix:
+    - Loop through the rows of the first matrix.
+    - Loop through the columns of the second matrix.
+    - Within each combination of row and column, use another loop to go through elements for multiplication and addition.
+    The code uses three nested loops (i, j, and k) to iterate through the matrices and calculate the resulting matrix element.
+*/ 
+
+
 #include <iostream>
 using namespace std;
+
 class matrix
 {
-    int m[3][3] ;
-    public :
+    int m[3][3];
+
+public:
     void read()
     {
-        for (int i = 0 ; i < 3 ; i++)
+        cout << "Enter the matrix:" << endl;
+        for (int i = 0; i < 3; i++)
         {
-            for(int j=0;j<3;j++)
+            for (int j = 0; j < 3; j++)
             {
-                cout << "Enter the matrix["<<i<<"]["<<j<<"] : ";
-                cin>>m[i][j];
+                cout << "Enter element [" << i << "][" << j << "]: ";
+                cin >> m[i][j];
             }
         }
     }
+
     void display()
     {
-        for (int i = 0 ; i < 3 ; i++)
+        cout << "Matrix:" << endl;
+        for (int i = 0; i < 3; i++)
         {
-            for(int j=0;j<3;j++)
+            for (int j = 0; j < 3; j++)
             {
-                cout << "\t**************** The matrix is ****************\n";
-                cout << "\t"<< m[i][j] ;
+                cout << "\t" << m[i][j];
             }
-            cout << "\n" ; 
+            cout << endl;
         }
     }
-    friend class multiplyMatrix ;
+
+    friend matrix multiplyMatrix(const matrix &m1, const matrix &m2);
 };
+
+/*
+     Using reference variables in the program allows the multiplication function 
+     to work directly with the original matrices, avoiding errors, 
+     unintended behavior, and performance issues associated with passing by value.
+     Not using it would give us garbage value as the changes brought would not be 
+     reflected in the main function 
+*/ 
+matrix multiplyMatrix(const matrix &m1, const matrix &m2) 
+{
+    matrix result;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            result.m[i][j] = 0; // Initializing the result matrix element to zero
+            for (int k = 0; k < 3; k++)
+            {
+                result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    matrix m1, m2; // object creation of the matrix 
+    cout << "Enter values for the first matrix:" << endl;
+    m1.read();
+    cout << "Enter values for the second matrix:" << endl;
+    m2.read();
+
+    cout << "\nFirst Matrix:" << endl;
+    m1.display();
+
+    cout << "\nSecond Matrix:" << endl;
+    m2.display();
+
+    matrix result = multiplyMatrix(m1, m2);
+
+    cout << "\nMatrix Multiplication Result:" << endl;
+    result.display();
+
+    return 0;
+}
