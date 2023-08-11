133de7ca689185af52dd7c08838ad6fe49ebda7e
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-11 23:36:08 +0530

    Update Q2.cpp

--- Chapter/Ch6/Q2.cpp
+++ Chapter/Ch6/Q2.cpp
@@ -1,1 +1,19 @@
+/*  Define a class String that could work as an user-defined string type. 
+    Include constructors that will enable us to create uninitialized string
 
+   String s1; // string with length 0
+   and also to initialize an object with a string constant at the time of creation like
+
+   String s2("Well done!");
+
+   Include a function that adds two strings to make a third string. Note that the statement
+   s2 = s1;
+   will be perfectly reasonable expression to copy one string to another.
+
+   Write a complete program to test your class to see that it does the following tasks:
+
+      (a) Creates uninitialized string objects.
+      (b) Creates objects with string constants.
+      (c) Concatenates two strings properly.
+      (d) Displays a desired string object.
+*/
