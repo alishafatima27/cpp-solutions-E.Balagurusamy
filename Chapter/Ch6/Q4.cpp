b6a6b60340d757ba9c4f692e94dd76f1b6a6e034
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-15 23:42:16 +0530

    Create Q4.cpp

---
+++ Chapter/Ch6/Q4.cpp
@@ -0,0 +1,7 @@
+/*  Improve the system design in Exercise 6.3 to incorporate the following features:
+       (a) The price of the books should be updated as and when required. 
+           Use a private member function to implement this.
+       (b) The stock value of each book should be automatically updated as soon as the transaction is completed.
+       (c) The number of successful and unsuccessful transactions should be recorded for the purpose of statistical analysis.
+           Use static data members to keep count of transactions.
+  */
