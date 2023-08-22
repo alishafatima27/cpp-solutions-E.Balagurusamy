9ea286372da7f85efa53e51067a7b6e12417d8e7
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-22 23:56:29 +0530

    Create Q3.cpp

---
+++ Chapter/Ch8/Q3.cpp
@@ -0,0 +1,27 @@
+/* An educational institution wishes to maintain a database of its employees. 
+   The database is divided into a number of classes whose hierarchical relationships are shown in Fig. 8.14. 
+                     __________
+                    | Staff    |
+                    |__________|
+                    | code name|
+                     __________
+                      /  |  \
+                    /    |    \
+                  /      |      \
+       ___________   ______   _______            
+      |    Teacher| |Typist| |Officer|
+      |___________| |______| |_______|
+      |   Subject | | speed| | grade |
+      |publication| |______| |_______|
+      |___________|  /  \
+                   /      \
+             ________     ________       
+            | Regular|   |Casual  |
+            |________|   |________|            
+                         | Daily  |
+                         |________|
+                        
+                    
+   The figure also shows the minimum information required for each class. 
+   Specify all the classes and define functions to create the database and retrieve individual information as and when required.
+*/
