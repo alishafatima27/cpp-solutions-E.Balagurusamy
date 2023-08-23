7f8240706575f69669fe959fb6bd01de62cf7bb2
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-23 23:09:53 +0530

    Create Q4.cpp

---
+++ Chapter/Ch8/Q4.cpp
@@ -0,0 +1,8 @@
+/* The database created in Exercise 8.3 does not include educational information of the staff. 
+   It has been decided to add this information to teachers and officers (and not for typists)
+   which will help the management in decision making with regard to training, promotion, etc. 
+   Add another data class called education that holds two pieces of educational information, namely, 
+   highest qualification in general education and highest professional qualification. 
+   This class should be inherited by the classes teacher and officer. 
+   Modify the program of Exercise 8.3 to incorporate these additions.
+*/
