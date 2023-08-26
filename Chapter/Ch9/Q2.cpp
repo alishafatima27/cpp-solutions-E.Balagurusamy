614f12e4d7b94cde1a2fcc618adc190a0b1c7f59
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-26 23:59:34 +0530

    Create Q2.cpp

---
+++ Chapter/Ch9/Q2.cpp
@@ -0,0 +1,7 @@
+/*  
+  Extend the previous program to display the area of circles. 
+  This requires addition of a new derived class 'circle' that computes the area of a circle. 
+  Remember, for a circle we need only one value, its radius, but the get_data() function in
+  the base class requires two values to be passed. 
+  (Hint: Make the second argument of get_data() function as a default one with zero value.)
+*/
