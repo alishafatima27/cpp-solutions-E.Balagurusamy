028d55c29474c5917239e76e96a80003112911d9
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-26 23:46:23 +0530

    Create Q7.cpp

---
+++ Chapter/Ch8/Q7.cpp
@@ -0,0 +1,4 @@
+/* We have learned that OOP is well suited for designing simulation programs. 
+   Using the techniques and learnt so far, design a program that would simulate 
+   a simple real-world system familiar to you.
+*/ 
