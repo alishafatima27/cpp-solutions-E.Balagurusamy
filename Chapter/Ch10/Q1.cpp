bc24c7e9202b35ec130befffa441d38027e9750f
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-30 00:00:34 +0530

    Update Q1.cpp

--- Chapter/Ch10/Q1.cpp
+++ Chapter/Ch10/Q1.cpp
@@ -9,3 +9,62 @@
   |______________________|______________________|_____ _________________|
   
   */ 
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Item
+{
+    string name;
+    int code;
+    float pr;
+
+public:
+    void getdata()
+    {
+        cout << "Enter name : ";
+        cin >> name;
+        cout << "Enter code : ";
+        cin >> code;
+        cout << "Enter price : ";
+        cin >> pr;
+    }
+
+    void display()
+    {
+        cout << left << setw(20) << setfill(' ') << name
+             << left << setw(20) << setfill(' ') << code
+             << fixed << setprecision(2) << left << setw(20) << setfill(' ') << pr
+             << endl;
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter number of items : ";
+    cin >> n;
+    Item *items[n];
+    for (int i = 0; i < n; i++)
+    {
+        items[i] = new Item();
+        items[i]->getdata();
+    }
+
+    cout << "-----------------------------------------------------------------------" << endl;
+    cout << "|         NAME         |         CODE         |         COST          |" << endl;
+    cout << "-----------------------------------------------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        items[i]->display();
+        delete items[i]; // Free memory
+    }
+
+    cout << "-----------------------------------------------------------------------" << endl;
+
+    return 0;
+}
+
