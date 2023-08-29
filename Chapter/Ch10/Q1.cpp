c4e79e3f11018cd46cf4a706407788c0ee98a977
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-08-29 23:14:19 +0530

    Create Q1.cpp

---
+++ Chapter/Ch10/Q1.cpp
@@ -0,0 +1,11 @@
+/* Write a program to read a list containing item name , item code , and cost 
+   interactively and prduce a three column output as shown below.
+   _____________________________________________________________________
+  |         NAME         |         CODE         |         COST          |
+  |______________________|______________________|_______________________|
+  |      Turbo C++       |         1001         |         250.95        |
+  |      C Primer        |         905          |         95.70         |
+  |       ......         |          ...         |          ...          |
+  |______________________|______________________|_____ _________________|
+  
+  */ 
