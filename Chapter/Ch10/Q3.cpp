9c713d7208e0d1c508a278213893442cb0f1fdef
Alisha Fatima <123461327+alishafatima27@users.noreply.github.com>
2023-09-01 23:47:53 +0530

    Create Q3.cpp

---
+++ Chapter/Ch10/Q3.cpp
@@ -0,0 +1,6 @@
+/* Write a program which reads a text from keyboard and displays the following information on the screen in two columns : 
+        (a) Number of lines 
+        (b) Number of words 
+        (c) Number of characters 
+   Strings should be left justified and numbers should be right justified in a suitable field width.
+*/ 
